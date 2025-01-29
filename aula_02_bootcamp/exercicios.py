dd3b42d9da5cf8619793294e210eb1193d3d4ad3
Leandro <546585@ead.ufscar.br>
2025-01-29 14:45:57 -0300

    salvando exercicios

--- aula_02_bootcamp/exercicios.py
+++ aula_02_bootcamp/exercicios.py
@@ -1,8 +1,23 @@
 
 
 # 1.Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+resultado = numero_1 + numero_2
+print("O Resultado é : ", resultado)
+
+
 # 2.Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero_1 = int(input("Digite um número: "))
+resultado = numero_1 % 5
+print("O Resultado é : ", resultado)
+
+
+
+
 # 3.Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
 numero_1 = int(input("Digite um número: "))
 numero_2 = int(input("Digite outro número: "))
 resultado = numero_1 * numero_2
@@ -19,3 +34,6 @@ print(f" O Resultado é :",  resultado)
 
 
 # 5.Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero_1 =  int(input("Digite um número: "))
+resultado = numero_1 ** 2
+print(f" O Resultado é :",  resultado)
