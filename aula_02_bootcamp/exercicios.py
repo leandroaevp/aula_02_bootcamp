7468ceba9a91f27661b3dd322b8d93f4e253763b
Leandro <546585@ead.ufscar.br>
2025-01-29 14:16:34 -0300

    adicioando exericio 4

---
+++ aula_02_bootcamp/exercicios.py
@@ -0,0 +1,13 @@
+nome_aluno =  "            Leandro"
+
+print(type(nome_aluno))
+
+print(nome_aluno.strip())
+
+numero_1=  int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número "))
+
+
+resultado = numero_1//numero_2
+
+print(f" O Resultado é :",  resultado)
