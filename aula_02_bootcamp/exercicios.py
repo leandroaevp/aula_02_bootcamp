ecb4b8677672bd249fbff93268e60a5e08513490
Leandro <546585@ead.ufscar.br>
2025-01-29 14:26:44 -0300

    subindo exercícios

--- aula_02_bootcamp/exercicios.py
+++ aula_02_bootcamp/exercicios.py
@@ -1,13 +1,21 @@
-nome_aluno =  "            Leandro"
 
-print(type(nome_aluno))
 
-print(nome_aluno.strip())
+# 1.Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# 2.Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 3.Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+resultado = numero_1 * numero_2
+print("O Resultado é : ", resultado)
+
+
+
+# 4.Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 numero_1=  int(input("Digite um número: "))
 numero_2 = int(input("Digite outro número "))
-
-
 resultado = numero_1//numero_2
-
 print(f" O Resultado é :",  resultado)
+
+
+# 5.Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
