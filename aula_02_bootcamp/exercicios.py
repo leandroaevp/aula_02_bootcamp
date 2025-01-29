abdcf8dea712eda605e48a260c43b862ce98fbfd
Leandro <546585@ead.ufscar.br>
2025-01-29 15:58:27 -0300

    salvando exercicios

--- aula_02_bootcamp/exercicios.py
+++ aula_02_bootcamp/exercicios.py
@@ -6,38 +6,27 @@ numero_2 = int(input("Digite outro número: "))
 resultado = numero_1 + numero_2
 print("O Resultado é : ", resultado)
 
-
 # 2.Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-
 numero_1 = int(input("Digite um número: "))
 resultado = numero_1 % 5
 print("O Resultado é : ", resultado)
 
-
-
-
 # 3.Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-
 numero_1 = int(input("Digite um número: "))
 numero_2 = int(input("Digite outro número: "))
 resultado = numero_1 * numero_2
 print("O Resultado é : ", resultado)
 
-
-
 # 4.Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-
 numero_1=  int(input("Digite um número: "))
 numero_2 = int(input("Digite outro número "))
 resultado = numero_1//numero_2
 print(f" O Resultado é :",  resultado)
 
-
 # 5.Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 numero_1 =  int(input("Digite um número: "))
 resultado = numero_1 ** 2
 print(f" O Resultado é :",  resultado)
-
 
 # 6.Escreva um programa que receba dois números flutuantes e realize sua adição
 numero_1 = float(input("Digite um numero: "))
@@ -51,7 +40,6 @@ numero_2 = float(input("Digite outro numero: "))
 resultado = (numero_1 + numero_2)/2
 print("O resultado é : ", resultado)
 
-
 # 8.Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 base = float(input("Digite um numero: "))
 expoente  = float(input("Digite outro numero: "))
@@ -62,7 +50,6 @@ print("O resultado é : " , resultado)
 temperatura_celsius = float(input("Digite a temperatura: "))
 conversao = (temperatura_celsius * 1.8 +32)
 print(" O resultado é : ", conversao)
-
 
 # 10.Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 raio  = float(input("Digite o raio do círculo: "))
@@ -75,8 +62,27 @@ resultado = nome.upper()
 print(" O resultado é : ", resultado)
 
 
-
 # 12.Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 nome_completo = input("Digite seu nome completo : ")
 resultado = nome_completo.lower()
 print("O Resultado é : ", resultado)
+
+# 13.Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+frase = input("Digite uma frase: ")
+resultado = frase.strip()
+print("O Resultado é : ", resultado)
+
+# 14.Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente
+data = input("Digite uma data no formato dd/mm/aaaa: ")
+dia , mes , ano = data.split("/")
+print(f"Dia: {dia}")
+print(f"Mês: {mes}")
+print(f"Ano: {ano}")
+
+# 15.Escreva um programa que concatene duas strings fornecidas pelo usuário
+nome = input("Digite o nome: ")
+sobrenome = input("Digite o sobrenome")
+resultado = nome + " " + sobrenome
+print("O resultado é : ", resultado)
+
+ol
